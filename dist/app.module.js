028930cfc92e0ee9d9a9962ea535c7f519c95c3c
Francois THIERREE <ftthierree@gmail.com>
2021-05-26 16:31:32 +0200

    added some modules in order to connect to db

--- dist/app.module.js
+++ dist/app.module.js
@@ -10,11 +10,14 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const config_1 = require("@nestjs/config");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [],
+        imports: [
+            config_1.ConfigModule.forRoot()
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+	  ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
