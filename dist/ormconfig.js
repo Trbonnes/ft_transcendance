4adfa1053b9459c418fb05c47342642cf926837b
Francois THIERREE <ftthierree@gmail.com>
2021-05-26 15:15:15 +0200

    no more wtf packages

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,1 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+    ],
+    synchronize: true,
+};
+exports.default = config;
 //# sourceMappingURL=ormconfig.js.map
