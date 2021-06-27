d0b2456cdcdd75130ff0a7da81930ce6bb023b29
Martin CLAUDEL <mclaudel@e2r4p16.42.fr>
2021-06-27 15:01:10 +0200

    added small sub menu for friends list

--- client/components/SideMenu/OnlineFriends.vue
+++ client/components/SideMenu/OnlineFriends.vue
@@ -1,13 +1,19 @@
 <template>
 <div>
-    <div class="flex flex-row items-center justify-start m-5 hover:bg-gray-500" v-for="f in friends">
-        <div>
+    <div class="relative flex flex-row items-center justify-between m-5 hover:bg-gray-500 w-11/12" v-for="f in friends" @mouseleave="active = null">
+        <div class="flex flex-row items-center justify-between">
             <div class="avatar relative m-1">
                 <div class="connection" :class="{connected : f.connected}" ></div>
             </div>
+            <span>{{f.name}}</span>
         </div>
-        <span>{{f.name}}</span>
-    </div>
+        <font-awesome-icon class="m-5 block cursor-pointer" @click="active = f.id" icon="ellipsis-v"></font-awesome-icon>
+        <div class="p-5 bg-gray-700 flex flex-col items-start justify-center text-white absolute right-0 top-full z-40" :class="{hidden: active != f.id}">
+            <span>send message</span>
+            <span>send invite</span>
+            <span>remove friend</span>
+        </div>
+    </div> 
 </div>
 
 </template>
@@ -16,10 +22,11 @@ import Vue from 'vue'
 export default Vue.extend({
 	data() {
 		return {
+            active : null, // will hold the id of the friend which is selected
 			friends : [
-				{name: "Bob", connected: true, status: "Don't worry be happy"},
-				{name: "Marcel", connected: true, status: "It's time to get funky"},
-				{name: "Giovanni", connected: false, status: "Everybody calls me Giorgio"},
+				{id: 1, name: "Bob", connected: true, status: "Don't worry be happy"},
+				{id: 2, name: "Marcel", connected: true, status: "It's time to get funky"},
+				{id: 3, name: "Giovanni", connected: false, status: "Everybody calls me Giorgio"},
 			]
 		}
 	},
