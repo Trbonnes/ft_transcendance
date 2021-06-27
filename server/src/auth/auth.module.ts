3c1f4d47d59ab71e3927f8f7015e507b15594f53
François Thierrée <francisco@kbn.home>
2021-06-28 00:50:17 +0200

    created auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { UsersModule } from '../users/users.module';
 import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forFeature([User])],
-  providers: [AuthService,  UsersService]
+  imports: [UsersModule, TypeOrmModule.forFeature([User]), PassportModule],
+  providers: [AuthService,  UsersService, LocalStrategy]
 })
 export class AuthModule {}
