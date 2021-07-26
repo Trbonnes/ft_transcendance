e6f974726669eabe068d484988a5fe817abe5e2d
Tristan BONNESON <trbonnes@e1r11p16.42.fr>
2021-07-26 15:05:27 +0200

    merging phaser

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,17 +6,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import * as Joi from '@hapi/joi';
 import { configService } from './config/config.service';
 import { UsersModule } from './users/users.module';
-<<<<<<< HEAD
 // import { ChatGateway, GameGateway } from './app.gateway';
 import { AuthModule } from './auth/auth.module';
 import { FooModule } from './foo/foo.module';
 import { AuthService } from './auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
-=======
 import { GameGateway } from './app.gamegateway';
 import { BorderlessGateway } from './app.borderlessgateway';
 import { ChatGateway } from './app.chatgateway';
->>>>>>> phaser
 
 @Module({
   imports: [
@@ -26,10 +23,6 @@ import { ChatGateway } from './app.chatgateway';
 	FooModule,
   ],
   controllers: [AppController],
-<<<<<<< HEAD
-  providers: [AppService, /*GameGateway, ChatGateway*/],
-=======
   providers: [AppService, GameGateway, ChatGateway, BorderlessGateway],
->>>>>>> phaser
 })
 export class AppModule {}
