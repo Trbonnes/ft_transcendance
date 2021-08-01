d6e11ecd94e341d51d47b714ff7046299eb9a732
cchenot <cchenot@student.42.fr>
2021-08-01 11:29:03 +0200

    [auth/back] with the controller its better

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthController } from './auth.controller';
+import { AuthController } from './auth.controller';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
