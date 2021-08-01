3dfec1f0e593a550e3f25266855b7e9b6d6d18b3
cchenot <cchenot@student.42.fr>
2021-08-01 11:31:52 +0200

    [auth/back] updated controller before break

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+	constructor(private readonly authService: AuthService,
+				private readonly userService: UsersService,
+				private readonly jwtService: JwtService) {}
+
+	
+
+}
