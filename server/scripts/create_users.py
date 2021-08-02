4b9d1ed4de9b799fb6b8138a4e430bf806cd50e3
yorn <yornclaudel@gmail.com>
2021-08-02 19:12:07 +0200

    modified script to fit new model

--- server/scripts/create_users.py
+++ server/scripts/create_users.py
@@ -2,7 +2,7 @@ import requests as rq
 from collections import namedtuple
 
 baseurl = 'http://localhost:3000'
-endpoint = '/users/register'
+endpoint = '/auth/testuser'
 
 users = list()
 usernames = [ "mario", "luigi", "dimitri", "albert", "gaben", "bowser", "qwerty", "azerty", "mireille", "dylan"]
@@ -10,8 +10,9 @@ usernames = [ "mario", "luigi", "dimitri", "albert", "gaben", "bowser", "qwerty"
 fails = 0
 
 for n in usernames:
-    rep = rq.post(baseurl + endpoint, data = { "email" : n + '@' + n, "password" : "123", "name" : n })
-    if rep.status_code != 201:
+    #rep = rq.post(baseurl + endpoint, data = { "email" : n + '@' + n, "password" : "123", "name" : n })
+    rep = rq.post(baseurl + endpoint + "?user=" + n)
+    if rep.status_code != 200:
         fails += 1
         print("Can't add user " + n + " error : ", rep.status_code)
 
