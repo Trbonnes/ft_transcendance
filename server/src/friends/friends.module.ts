f7548db130cbc7bfa9e4f6837c58bb4b8d621710
cchenot <cchenot@student.42.fr>
2021-08-04 00:00:51 +0200

    [Friend/back] saving, wip, friendsService left

--- server/src/friends/friends.module.ts
+++ server/src/friends/friends.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FriendRequest } from 'src/entities/friend-request.entity';
+import { UsersModule } from 'src/users/users.module';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
 
 @Module({
+  imports: [ UsersModule, TypeOrmModule.forFeature([FriendRequest])],
   controllers: [FriendsController],
   providers: [FriendsService]
 })
