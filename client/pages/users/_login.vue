4f890b6d3414b3231ae0dde4211e86062e9c6d7d
cchenot <cchenot@student.42.fr>
2021-08-05 18:45:07 +0200

    [user/front] basic profile page done

--- client/pages/users/_login.vue
+++ client/pages/users/_login.vue
@@ -2,10 +2,23 @@
 	<div>
 		<section class="body-font">
   			<div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    			<img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded " alt="hero" :src="user.avatar">
+    			<img class="lg:w-1/6 md:w-2/6 w-4/6 mb-10 object-cover object-center rounded " alt="hero" :src="user.avatar">
     				<div class="text-center lg:w-2/3 w-full">
-      					<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">Hello {{ user.displayName }} !</h1>
-      						<p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+      					<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">Challenger {{ user.displayName }} !</h1>
+					<div class="flex flex-1 flex-row m-5 justify-center">
+							<div class="card shadow compact m-3">
+  								<div class="card-body">
+ 								   <h2 class="card-title">Victories</h2> 
+   										<p>{{ user.victory }}</p>
+  								</div>
+							</div> 
+							<div class="card shadow compact m-3">
+  								<div class="card-body">
+    								<h2 class="card-title">Defeats</h2> 
+    								<p>{{ user.defeat }}</p>
+  								</div>
+							</div>
+					</div>
       							<div class="flex justify-center">
         							<button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
         								<button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
