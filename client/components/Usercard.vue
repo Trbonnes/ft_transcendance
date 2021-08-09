a3b53f921bdbfe9d7f35b98d8a2117e5b8da8c1f
chris <chris@chris.com>
2021-08-10 00:13:00 +0200

    started leaderboard / usercard

---
+++ client/components/Usercard.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<div class="card shadow">
+		  <div class="card-body">
+		    <h2 class="card-title">no border with shadow</h2> 
+		    <p>Rerum reiciendis beatae tenetur excepturi</p>
+		  </div>
+		</div> 
+	</div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import {Component, Prop} from "nuxt-property-decorator"
+
+	@Component
+	export default class UserCard extends Vue {
+		@Prop({required: true}) user: any
+		
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/leaderboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import {Component} from 'nuxt-property-decorator'
+
+	@Component(
+		{
+			middleware: ['auth']
+		}
+	)
+	export default class Leaderboard extends Vue {
+		users: any[] = []
+		
+		async fetch() {
+			this.users = await this.$axios.$get('/users')
+		}
+
+		get sortedUsers() : any[] {
+			return this.users.sort((a, b) => {
+				return (a.victory < b.victory ? 1 : -1)
+			})
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
