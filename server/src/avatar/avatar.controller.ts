9afdf4e453e567886cf54fe89b9a305f43072994
cchenot <cchenot@student.42.fr>
2021-08-19 23:08:14 +0200

    [back/avatar] updated

--- server/src/avatar/avatar.controller.ts
+++ server/src/avatar/avatar.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AvatarService } from './avatar.service';
+import { Express, request, Response } from "express";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from "multer"
+import { fstat } from 'fs';
 
 @Controller('avatar')
-export class AvatarController {}
+export class AvatarController {
+	constructor(private avatarService: AvatarService) {}
+
+	@Get(":file")
+	@UseGuards(JwtAuthGuard)
+	async findOne(@Param('file') file:string, @Res() res: Response) {
+		const fs = require("fs");
+		if (fs.existsSync(`./uploads/${file}`))
+			return res.sendFile(`${file}`, {root: `./uploads`})
+		return null;
+	}
+
+	@Post(":id")
+	@UseGuards(JwtAuthGuard)
+	@UseInterceptors(
+		FileInterceptor('file', {
+			storage: diskStorage({
+				destination: './uploads',
+				filename: avatarFileName
+			}),
+			fileFilter: isImageFilter,
+			limits: {fileSize: 2097152}
+		})
+	)
+	addAvatar(@Param('id') id: string, @UploadedFile() file: Express.Multer.File) {
+		return "OK"
+	}
+
+//	@Delete(":id")
+//	@UseGuards(JwtAuthGuard)
+//	async remove(@Param('id') id:string) {
+//		return this.avatarService.deleteFile(id)
+//	}
+}
--- server/src/avatar/avatar.service.ts
+++ server/src/avatar/avatar.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { unlink } from 'fs';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AvatarService {}
+export class AvatarService {
+
+	constructor(private usersService: UsersService) {}
+	
+	private logger: Logger = new Logger()
+
+//	deleteFile(login: string): string {
+//		const fs = require("fs")
+//		if (fs.existsSync(`./uploads/${login}`)) {
+//			unlink(`./uploads/${login}`, () => {
+//				this.logger.log(`${login} avatar deleted from storage`)
+//			})
+//			return "OK";
+//		return "FAIL";
+//	}
+//
+}
