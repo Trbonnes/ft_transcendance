b336302dd4ae768973103632381bd7254e597c1a
yorn <yornclaudel@gmail.com>
2021-09-28 22:35:53 +0200

    fixing direct channel

--- client/components/chat/DirectSingle.vue
+++ client/components/chat/DirectSingle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full">
-        <ChatConversation :messages="getMessages" @sendMessage="sendMessage" />
+        <ChatConversation :members="getMembers" :messages="getMessages" @sendMessage="sendMessage" />
         <div class="w-full">
           <span @click="defyUser" class="btn m-1">
               Defy
@@ -12,29 +12,35 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    props : ["userId", "channelId"],
+    props : ["channel"],
     async fetch() {
-        await this.$store.dispatch('directChannel/history', this.channelId)
+        await this.$store.dispatch('directChannel/history', this.channel.id)
     },
     computed : {
         getMessages()
         {
-          let data = this.$store.getters["directChannel/messages"](this.channelId)
+          let data = this.$store.getters["directChannel/messages"](this.channel.id)
           console.log('Here are the message ', data)
           return data
         },
+        getMembers()
+        {
+          if (this.$auth.user)
+            return [ this.channel.user , this.$auth.user ]
+          return []
+        }
     },
     methods : {
         defyUser()
         {
-          this.$router.push(`/game?friendId=${this.userId}`)
+          this.$router.push(`/game?friendId=${this.channel.user.id}`)
           this.$emit("hide")
         },
         async sendMessage(content : string)
         {
           try
           {
-            let data = await this.$store.dispatch('directChannel/sendMessage',  { userId : this.userId, content : content })
+            let data = await this.$store.dispatch('directChannel/sendMessage',  { userId : this.channel.user.id, content : content })
           }
           catch(error : any)
           {
